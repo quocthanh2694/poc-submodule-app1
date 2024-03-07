9e1476bb5513914aa46ecbf3e18f960354c202aa
thanhtran <quocthanh2694@gmail.com>
2024-03-07 09:49:59 +0700

    Initial commit

---
+++ app1/src/app/page.tsx
@@ -0,0 +1,17 @@
+import styles from './page.module.scss';
+import { H1 } from '@poc-submodule/ui';
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <div className={styles.page}>
+      App 1
+      <br />
+      <H1></H1>
+    </div>
+  );
+}
